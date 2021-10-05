769b44dc7dd03b9a93c1d59d1f22de281499fdde
chihabsd <adamchihab2012@hotmail.com>
2021-10-05 08:30:54 +0100

    first commit with fonts, images setup

---
+++ src/screens/Test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import {FONTS} from '../constants';
+const Test = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text style={{...FONTS.body1}}> Testing the app </Text>
+    </View>
+  );
+};
+
+export default Test;
